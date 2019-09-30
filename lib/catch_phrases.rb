fb9aa6fbcd534f443b2e8d9198e1e1cbf5128b9b
Pam Cairo <pcairo_90@outlook.com>
2019-09-30 11:10:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  local_variables= 'It's-a me, Mario!'
+  puts local_variables
+end
 end
 
 def toadstool
   puts status
-end
+  end
+
